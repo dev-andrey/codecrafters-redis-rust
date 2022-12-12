49bc38000737630184ef290ab34c27377312b6c6
Andrey Lebedev <5165852+dev-andrey@users.noreply.github.com>
2022-12-11 19:50:59 -0500

    pass 2nd stage

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
+use std::io::{Read, Write};
 use std::net::TcpListener;
+
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut stream) => {
                 println!("accepted new connection");
+
+                // wait for a message from the client and ignore the content
+                let mut buf = [0; 512];
+                stream.read(&mut buf).unwrap();
+
+                stream.write("+PONG\r\n".as_bytes()).unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
